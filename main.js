40d1f5cdbe22f0bbb9fa85b745e251709ad3bbf5
Desmond Williams <demowi8@gmail.com>
2022-05-02 05:19:34 -0400

    buttons edit and delete

--- main.js
+++ main.js
@@ -28,11 +28,34 @@ window.addEventListener('loads', () => {
         // ** append child node to its parent node
         task_el.appendChild(task_content_el);
 
+        // ** create task input element
         const task_input_el = document.createElement('input');
         task_input_el.type = 'text';
         text_input_el.value = task;
         task_input_el.setAttribute('readonly', 'readonly')
 
         task_content_el.appendChild(task_input_el);
+
+        // ** Buttons!
+        const task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
+        //* Edit BTN
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerHTML = 'Edit';
+        //* Delete BTN
+        const task_delete_el = document.createElement('button');
+        task_delete_el.add('delete');
+        task_delete_el.innerHTML = 'Delete';
+
+        // ** Append btns to action element
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        // * append action elements to task element
+        task_el.appendChild(task_actions_el);
+
+        list_el.appendChild(task_el);
+
     })
 })
